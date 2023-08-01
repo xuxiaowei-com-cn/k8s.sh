b9d74453628c44c1646c9ab40c4e4a28d41dcf1c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-08-01 12:52:28 +0800

    :memo: 废弃说明

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # 使用：chmod +x install.sh && sudo ./install.sh
 # 仓库：https://jihulab.com/xuxiaowei-com-cn/k8s.sh
 #
-# 当前文件已被废弃，请查看新文件
+# 当前文件已被废弃，请查看新文件（新版差异较大，前后不兼容）
 # 新文件链接：https://jihulab.com/xuxiaowei-com-cn/k8s.sh/-/blob/main/k8s.sh
 # 新文件下载链接：https://jihulab.com/xuxiaowei-com-cn/k8s.sh/-/raw/main/k8s.sh
 
@@ -12,7 +12,7 @@ readonly COLOR_RED='\033[31m'
 readonly COLOR_GREEN='\033[92m'
 readonly COLOR_RESET='\033[0m'
 
-echo -e "${COLOR_RED}当前文件已被废弃，请使用新版本${COLOR_RESET}"
+echo -e "${COLOR_RED}当前文件已被废弃，请使用新版本（新版差异较大，前后不兼容）${COLOR_RESET}"
 echo ""
 echo -e "${COLOR_RED}新版本说明：${COLOR_RESET}${COLOR_GREEN}https://jihulab.com/xuxiaowei-com-cn/k8s.sh${COLOR_RESET}"
 echo -e "${COLOR_RED}新版本链接：${COLOR_RESET}${COLOR_GREEN}https://jihulab.com/xuxiaowei-com-cn/k8s.sh/-/blob/main/k8s.sh${COLOR_RESET}"
