9b9d1efc411c1968064f5ad424b071b9f5890f64
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-08-02 20:57:48 +0800

    :construction_worker: 调整检查时间间隔、增加颜色

--- check.sh
+++ check.sh
@@ -1,3 +1,8 @@
+# 颜色定义
+readonly COLOR_BLUE='\033[34m'
+readonly COLOR_GREEN='\033[92m'
+readonly COLOR_RESET='\033[0m'
+
 echo "$KUBECONFIG"
 
 kubectl get pod --all-namespaces -o wide
@@ -8,7 +13,7 @@ count=0
 max_attempts=100
 
 while [ $count -lt $max_attempts ]; do
-  echo "检查 pod 状态..."
+  echo "${COLOR_BLUE}检查 pod 状态...${COLOR_RESET}"
   kubectl get pod --all-namespaces
   all_running=true
 
@@ -24,7 +29,7 @@ while [ $count -lt $max_attempts ]; do
   done <<<"$pod_status"
 
   if $all_running; then
-    echo "所有 pods 全部运行"
+    echo "${COLOR_GREEN}所有 pods 全部运行${COLOR_RESET}"
     break
   fi
 
@@ -34,5 +39,5 @@ while [ $count -lt $max_attempts ]; do
     break
   fi
 
-  sleep 3
+  sleep 10
 done
