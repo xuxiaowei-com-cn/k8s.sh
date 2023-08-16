dc34fa8f51fbdac39ecfc976ea8a28d93315c12f
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-08-16 08:29:00 +0800

    :hammer: 新增 k8s 1.28.0 支持

--- k8s.sh
+++ k8s.sh
@@ -29,7 +29,7 @@ readonly kernel_target_version=3.10.0-957.el7.x86_64
 readonly lower_major=1
 readonly lower_minor=24
 readonly upper_major=1
-readonly upper_minor=27
+readonly upper_minor=28
 
 # 高可用主节点地址
 availability_master_array=()
