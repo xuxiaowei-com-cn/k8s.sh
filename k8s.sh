b01a5a01c25bb93a5b2d6284324be7d765acd0eb
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-10-18 19:36:15 +0800

    :sparkles: 为服务（Service）的虚拟 IP 地址另外指定 IP 地址段 https://github.com/xuxiaowei-com-cn/k8s.sh/issues/2 https://jihulab.com/xuxiaowei-com-cn/k8s.sh/-/issues/36 https://jihulab.com/xuxiaowei-com-cn/k8s.sh/-/issues/55

--- k8s.sh
+++ k8s.sh
@@ -893,27 +893,31 @@ _kubernetes_init() {
       init_node_name="--node-name=$node_name"
     fi
 
+    if [[ $service_cidr ]]; then
+      init_service_cidr="--service-cidr=$service_cidr"
+    fi
+
     if [[ $availability_vip ]]; then
       if [ "$kubernetes_version" ]; then
         echo -e "${COLOR_BLUE}kubernetes 高可用 VIP ${COLOR_RESET}${COLOR_GREEN}${availability_vip}${COLOR_RESET}${COLOR_BLUE} 初始化时使用的镜像版本 ${COLOR_RESET}${COLOR_GREEN}${kubernetes_version}${COLOR_RESET}"
         # 此处的 $init_ 开头的参数不能带引号
-        kubeadm init $init_v $init_api_address $init_api_port $init_node_name --image-repository=registry.aliyuncs.com/google_containers --kubernetes-version=v"$kubernetes_version" --control-plane-endpoint "$availability_vip:9443" --upload-certs
+        kubeadm init $init_v $init_api_address $init_api_port $init_node_name $init_service_cidr --image-repository=registry.aliyuncs.com/google_containers --kubernetes-version=v"$kubernetes_version" --control-plane-endpoint "$availability_vip:9443" --upload-certs
       else
         # https://cdn.dl.k8s.io/release/stable-1.txt
         echo -e "${COLOR_BLUE}kubernetes 高可用 VIP ${COLOR_RESET}${COLOR_GREEN}${availability_vip}${COLOR_RESET}${COLOR_BLUE} 初始化时使用当前次级版本最新镜像（自动联网获取版本号）${COLOR_RESET}"
         # 此处的 $init_ 开头的参数不能带引号
-        kubeadm init $init_v $init_api_address $init_api_port $init_node_name --image-repository=registry.aliyuncs.com/google_containers --control-plane-endpoint "$availability_vip:9443" --upload-certs
+        kubeadm init $init_v $init_api_address $init_api_port $init_node_name $init_service_cidr --image-repository=registry.aliyuncs.com/google_containers --control-plane-endpoint "$availability_vip:9443" --upload-certs
       fi
     else
       if [ "$kubernetes_version" ]; then
         echo -e "${COLOR_BLUE}kubernetes 初始化时使用的镜像版本 ${COLOR_RESET}${COLOR_GREEN}${kubernetes_version}${COLOR_RESET}"
         # 此处的 $init_ 开头的参数不能带引号
-        kubeadm init $init_v $init_api_address $init_api_port $init_node_name --image-repository=registry.aliyuncs.com/google_containers --kubernetes-version=v"$kubernetes_version"
+        kubeadm init $init_v $init_api_address $init_api_port $init_node_name $init_service_cidr --image-repository=registry.aliyuncs.com/google_containers --kubernetes-version=v"$kubernetes_version"
       else
         # https://cdn.dl.k8s.io/release/stable-1.txt
         echo -e "${COLOR_BLUE}kubernetes 初始化时使用当前次级版本最新镜像（自动联网获取版本号）${COLOR_RESET}"
         # 此处的 $init_ 开头的参数不能带引号
-        kubeadm init $init_v $init_api_address $init_api_port $init_node_name --image-repository=registry.aliyuncs.com/google_containers
+        kubeadm init $init_v $init_api_address $init_api_port $init_node_name $init_service_cidr --image-repository=registry.aliyuncs.com/google_containers
       fi
     fi
 
@@ -1323,6 +1327,10 @@ while [[ $# -gt 0 ]]; do
 
   node-name=* | -node-name=* | --node-name=*)
     node_name="${1#*=}"
+    ;;
+
+  service-cidr=* | -service-cidr=* | --service-cidr=*)
+    service_cidr="${1#*=}"
     ;;
 
   calico-version=* | -calico-version=* | --calico-version=*)
